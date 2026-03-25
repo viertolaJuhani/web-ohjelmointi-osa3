5884b8a501125fb3971c3ab6c728f1cf1326082d
Juhani Viertola <javier@utu.fi>
2026-03-25 23:47:19 +0200

    fixed addPerson notification bug

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
-const app = express()
 const Person = require('./models/person')
+
+const app = express()
+
+const errorHandler = (error, request, response, next) => {
+  console.log(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
 
 app.use(express.static('dist'))
 app.use(express.json())
@@ -13,18 +24,17 @@ app.get('/info', (request, response) => {
   Person.find({}).then(persons => {
     const count = persons.length
   
-    response.send(`
-      <p>Phonebook has info for ${count} people</p>
-      <p>${timeNow}</p>
-  `)
+    response.send(
+      `<p>Phonebook has info for ${count} people</p>
+      <p>${timeNow}</p>`
+    )
   })
 })
 
-app.get('/api/persons', (request, response, next) => {
+app.get('/api/persons', (request, response) => {
   Person.find({}).then(people => {
     response.json(people)
   })
-  .catch(error => next(error))
 })
 
 app.get('/api/persons/:id', (request, response, next) => {
@@ -38,37 +48,40 @@ app.get('/api/persons/:id', (request, response, next) => {
   .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
-    .then(result => {
+    .then(() => {
       response.status(204).end()
     })
     .catch(error => next(error))
 })
 
-app.post('/api/persons', (request, response)  => {
+app.post('/api/persons', (request, response, next)  => {
   const body = request.body
   
   if (!body.name || !body.number) {
     return response.status(400).json({
       error: 'name or number missing'
     })
-  //} else {
-    //Person.find({}).then(people => {
-      //if (people.some(p => p.name === body.name)) {
-        //return response.status(400).json({
-          //error: 'name must be unique'
-        //})
-      //}
-    //})
   }
 
-  const person = new Person({
+  Person.find({}).then(people => {
+    if (people.some(p => p.name === body.name)) {
+      return response.status(400).json({
+        error: 'name must be unique'
+      })
+    }
+    const person = new Person({
     name: body.name,
     number: body.number,
   })
-  person.save().then(savedPerson => {
-    response.json(savedPerson)
+
+  person
+    .save()
+    .then(savedPerson => {
+      response.json(savedPerson)
+    })
+    .catch (error => next(error))
   })
 })
 
@@ -77,17 +90,6 @@ const unknownEndpoint = (request, response) => {
 }
 
 app.use(unknownEndpoint)
-
-const errorHandler = (error, request, response, next) => {
-  console.log(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  }
-
-  next(error)
-}
-
 app.use(errorHandler)
 
 const PORT = process.env.PORT
